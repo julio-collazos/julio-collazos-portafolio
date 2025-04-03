39c20b89a3a591b54ebe66136442f8e54cc8e96b
juliocollazos <julio.collazos@proforest.net>
2025-04-03 12:33:55 -0500

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/julio-collazos" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/julio-collazos/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>Portfolio of data analysis and geospatial services, created by <a href="https://www.linkedin.com/in/julio-collazos/" target="_blank" rel="noreferrer">Julio Collazos</a> to transform data into solutions 💜</p>
     </footer>
   );
 }
